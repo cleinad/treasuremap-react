b797516696c093a13e9bbc2c2370d89ce469e789
cleinad <danieltwentytwo@gmail.com>
2025-01-11 14:21:48 -0800

    Working Landing Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,28 @@ import Banner from '../components/Banner'
 const Home = () => {
   return (
     <>
+        <section className="hero-section">
+            <div className ="gradient"/>
+            <div className="hero-content">
+                <h1>Welcome, Adventurer</h1>
+                <h3 style={{color: "var(--background-color)"}}>Embark on a journey to conquer your financial fears and uncover life's greatest treasures.</h3>
+                <button className="cta-button">
+                    <a href="financial-foundation">Embark!</a>
+                </button>
+            </div>
+        </section>
+
+        {/*<!-- Regular content section -->*/}
+        <section className="info-section">
+            <h2>Chart Your Course</h2>
+            <p>Discover simple steps to navigate your financial journey, from understanding where you are today to preparing for a bright tomorrow.</p>
+            <p><br/>
+                Welcome to you personal finance map! Here, you'll uncover the secrets to financial treasure: 
+                the knowledge, tools and strategies to navigate life's adventures with confidence
+            </p>
+        </section>
+
         <Banner />
-        <div>Home</div>
     </>
   )
 }
