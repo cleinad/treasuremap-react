b4d34e8703237849570ec859c59c214b670705ea
cleinad <danieltwentytwo@gmail.com>
2025-01-15 01:08:14 -0800

    Updated footer and github case sensitivity bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const Footer = () => {
+interface Props {
+    nextPageLink: string;
+}
+
+const Footer = ({nextPageLink}:Props) => {
   return (
     <div className="next-page">
         {/*<Link to="/wealth">Onto the Next!</Link>*/}
